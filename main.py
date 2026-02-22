31f82eefc402eae4b6cca3a75c1d05b8e326a29f
rikaehler <richy.kaehler@gmail.com>
2026-02-22 15:29:13 +0100

    added clock and delta time

--- main.py
+++ main.py
@@ -5,11 +5,15 @@ from logger import log_state
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver} ")
     print(f"Screen width: {SCREEN_WIDTH}")
-    print (f"Screen height: {SCREEN_HEIGHT}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
     # initialize pygame and get new instance of GUI window
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    # create clock object and delta time
+    clock = pygame.time.Clock()
+    dt = 0
 
     #start game loop
     while True:
@@ -23,6 +27,8 @@ def main():
         # fill screen with solid black and refresh screen   
         screen.fill("black")
         pygame.display.flip
+        clock.tick(60)
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
