ad7ac0c61ffd3642c4d20f1a2c916c2190d9636f
rikaehler <richy.kaehler@gmail.com>
2026-02-22 15:16:30 +0100

    added begin/quit game loop to main

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 import pygame
-import constants
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver} ")
-    print(f"Screen width: {constants.SCREEN_WIDTH}")
-    print (f"Screen height: {constants.SCREEN_HEIGHT}")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print (f"Screen height: {SCREEN_HEIGHT}")
+
+    # initialize pygame and get new instance of GUI window
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    #start game loop
+    while True:
+        log_state()
+
+        # event game loop, exit loop when close window
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        # fill screen with solid black and refresh screen   
+        screen.fill("black")
+        pygame.display.flip
 
 if __name__ == "__main__":
     main()
